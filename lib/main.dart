3e7ee9ea1ab766bd96bdd90b1a93851ce9005f89
La Pyae <pyaeoo@bigsoft.tech>
2021-01-29 10:56:50 +0630

    'Testing'

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Testing',
+              'Testing 1',
             ),
             Text(
               '$_counter',
